39325eee5efb509ea2a18b3d938434d80b8f2bfd
lamaningma <lamaninma904@gmail.com>
2020-05-07 10:21:42 +0545

    Assignment3

--- Assignment3/Ningma.java
+++ Assignment3/Ningma.java
@@ -1,1 +1,19 @@
-public class Ningma{public static void main(String[] args){System.out.println("hello world: i am Ningma");}}
+public class Ningma
+{
+public static void main(String[] args){
+
+System.out.println("hello world: i am Ningma");
+
+System.out.println(“- - - - - - - - - -“);
+System.out.println(“|   @          @    |“);
+System.out.println(“|                   |”);
+System.out.println(“- - - - - - — - - - — -“);
+System.out.println("         /|\       “);
+
+System.out.println("         /|\       “);
+
+
+
+}
+
+}
