6b5789818deb353274c83595790d37ce3d73c71b
Sajnee Shrestha <shresthasajnee04@gmail.com>
2020-05-07 18:41:33 -0400

    sketch

---
+++ Assignment3/Human.java/Human.java
@@ -0,0 +1,12 @@
+public class Human{
+public static void main (String[]arg){
+System.out.println(" ------- ");
+System.out.println("| @   @ |");
+System.out.println("|       |");
+System.out.println(" ------- ");
+System.out.println("   /|\\  ");
+System.out.println("         ");
+System.out.println("   /|\\  ");
+}
+}
+
