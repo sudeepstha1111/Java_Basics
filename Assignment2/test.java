c0271b839ab45b7c1be4b84462c94e014e7d73ce
Roshan Shah <mrshahroshan@gmail.com>
2020-05-06 20:18:29 -0400

    Update test.java

--- Assignment2/test.java
+++ Assignment2/test.java
@@ -2,7 +2,7 @@ public class test{
 
 public static void main(String []args){
 
-System.out.println9"Hello World: This is my first Java Class");
+System.out.println("Hello World: This is my first Java Class");
 }
 }
 
