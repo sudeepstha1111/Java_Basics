4ab1c99df10bc1afbcfbaa4f2764fd27fff4b970
blockofcodes <mrshahroshan@gmail.comcleargit config --global user.email mrshahroshan@gmail.com>
2020-05-08 19:28:17 -0400

    assignment 4

---
+++ Assignment4/Roshan.java
@@ -0,0 +1,23 @@
+public class Roshan
+{
+public static void main(String[] args){
+byte Num = 1;
+short money  = 10;
+long Salary = 100L;
+boolean male = true;
+boolean female = false;
+int phone = 917821;
+float temp = 20.1f;
+char Alpha = 'A';
+double account = 12345;
+System.out.println("My fav num is" + Num);
+System.out.println("Currently I have " + money + "dollar in my pocket");
+System.out.println(" i am  male" + male);
+System.out.println("I love cat " + false);
+System.out.println("My phone number is" + phone);
+System.out.println("Todays Current Temperature is" + temp );
+System.out.println("The first letter in alphabet is" +  Alpha );
+System.out.println("My bank account is " + account );
+System.out.println("My annual salary is" + Salary);
+}
+}
