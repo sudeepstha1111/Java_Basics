2132b1f38d7a2e54536b97fede92f2837c708e4e
blockofcodes <mrshahroshan@gmail.comcleargit config --global user.email mrshahroshan@gmail.com>
2020-05-10 14:08:10 -0400

    QUestion 4 uploaded

---
+++ Assignment5/Roshan/four.java
@@ -0,0 +1,18 @@
+public class four
+{
+public static void main(String[] args){
+
+int number = 10;
+  
+System.out.println("The given number is " +number);
+
+if (number%2==0){
+
+System.out.println("The given number is even");
+}else{
+
+System.out.println("The given number is odd");
+
+}
+}
+}
