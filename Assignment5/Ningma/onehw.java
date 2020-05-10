c89aa21d112c444186dd8037a1bd6c85f1c4d34e
lamaningma <lamaninma904@gmail.com>
2020-05-11 01:32:51 +0545

    onehw.java

---
+++ Assignment5/Ningma/onehw.java
@@ -0,0 +1,12 @@
+public class onehw{
+public static void main(String[] args){
+int a = 4;
+int b = 2;
+
+System.out.println("sum of two numbers a and b is " + (a+b));
+System.out.println("The difference of two number is " + (a-b));
+System.out.println("Division of two number is " + ( a / b));
+System.out.println("Modulo of a and b is " + (a % b));
+System.out.println("Multiplication of a and b is " + (a * b));
+}
+}
