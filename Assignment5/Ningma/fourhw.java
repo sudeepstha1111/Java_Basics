bbeafc41078c0a4d795abd970e8428706a3395a4
lamaningma <lamaninma904@gmail.com>
2020-05-11 11:50:30 +0545

    assignment4

--- Assignment5/Ningma/fourhw.java
+++ Assignment5/Ningma/fourhw.java
@@ -1,9 +1,13 @@
 public class fourhw{
+
 public static void main(String [] args){
+
 int temp  = 21;
-if (temp % 2==0{
-System.out.println("Temp is even.");
+
+if (temp % 2==0) {
+System.out.println("Number is even.");
 } else {
-System.out.println("Temp is odd.");
+System.out.println("Number  is odd.");
 }
 } 
+}
