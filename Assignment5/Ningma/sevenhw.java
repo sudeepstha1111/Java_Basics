47da049ad9f890366e322759ed12efbf985e4ac6
lamaningma <lamaninma904@gmail.com>
2020-05-11 15:32:34 +0545

    assignment7

---
+++ Assignment5/Ningma/sevenhw.java
@@ -0,0 +1,21 @@
+public class sevenhw{
+
+public static void main(String[] args){
+
+int age = 31;
+
+if (age<=18)
+{
+System.out.println("You cannot drink nor smoke. You are under age");
+
+}else if(age>18 &&  age<=21) {
+
+System.out.println("You can smoke but cant drink. You will need to be over 21 to drink");
+
+}else{ 
+
+System.out.println("You can drink and smoke");
+}
+}
+}
+
