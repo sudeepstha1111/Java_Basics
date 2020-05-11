5c694136de7616cc55893bfffda6c01f487bf949
lamaningma <lamaninma904@gmail.com>
2020-05-11 11:16:21 +0545

    assignment4

---
+++ Assignment5/Ningma/fourhw.java
@@ -0,0 +1,10 @@
+public class fourhw{
+public static void main(String [] args){
+int temperature = 100;
+if (temperature < 98){
+System.out.println("patient has no fever.");
+} else {
+System.out.println("patient has fever.");
+}
+}
+} 
