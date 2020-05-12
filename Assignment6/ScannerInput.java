3bb55fc0c333a86a5a9a59b34f1196e8d9932769
sudeepstha1111 <sudeep.stha1111@gmail.com>
2020-05-11 20:56:24 -0400

    added assignment 6

---
+++ Assignment6/ScannerInput.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ScannerInput{
+
+public static void main(String args []){
+
+Scanner input = new Scanner(System.in);
+
+System.out.println("Welcome to my Program");
+System.out.println("Enter your age");
+
+//int age = 5;
+int age = input.nextInt();
+
+System.out.println("Enter your name");
+String name = input.nextLine();
+
+System.out.println("My age is : "+ age+ "  and My Name is : "+ name);
+
+
+
+}
+
+
+
+}
---
+++ Assignment6/SwitchCaseExample.java
@@ -0,0 +1,24 @@
+public class SwitchCaseExample{
+
+public static void main(String args[]){
+
+int i =9;
+
+switch(i){
+
+case 1: System.out.println("Case 1");
+break;
+case 2: System.out.println("Case 2");
+case 3: System.out.println("Case 3");
+case 4: System.out.println("Case 4");
+case 5: System.out.println("Case 5");
+break;
+case 6: System.out.println("Case 6");
+
+default: System.out.println("Case Default");
+}
+
+}
+
+
+}
