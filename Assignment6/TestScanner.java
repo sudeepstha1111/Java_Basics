d83d0024a6be13f47c6b0955690ada5d6e3d07eb
sudeepstha1111 <sudeep.stha1111@gmail.com>
2020-05-12 23:53:19 -0400

    update

---
+++ Assignment6/TestScanner.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class TestScanner{
+
+public static void main(String[] abc){
+
+ System.out.println("Welcome to Paris");
+ System.out.println("Hello ! How are you ? ");
+  
+ Scanner in = new Scanner(System.in);
+ String input = in.nextLine();
+ System.out.println("Hi !,My name is " +input + "I am doing great");
+
+}
+
+
+}
+
