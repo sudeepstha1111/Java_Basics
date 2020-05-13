437b62f6975052bf529b81f8a0b1ba3d1811405b
blockofcodes <mrshahroshan@gmail.comcleargit config --global user.email mrshahroshan@gmail.com>
2020-05-13 18:53:05 -0400

    Assignment 7 uploaded

---
+++ Assignment7/five.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class five {
+  public static void main(String[] args) {
+    Scanner cal = new Scanner(System.in);
+      System.out.println("Enter the Number to play even and odd game:");
+      int a = cal.nextInt();
+
+
+if (a%2==0){
+
+System.out.println("The number you have chose is even");
+}else{
+
+System.out.println("The number you have chose is odd");
+
+}
+
+}
+}
