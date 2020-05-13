d61a65c4aaa8148617c96c03b34c2532ee12f3f9
blockofcodes <mrshahroshan@gmail.comcleargit config --global user.email mrshahroshan@gmail.com>
2020-05-13 02:13:06 -0400

    Assignment 7

---
+++ Assignment7/Roshan/three.java
@@ -0,0 +1,15 @@
+public class three {
+
+public static void main(String[] args) {
+
+int num = 5;
+long fac = 1;
+
+ for(int i = 1; i <= num; ++i)
+        {
+         fac = fac * i;
+        }
+
+        System.out.println("The Factorial of 5 is  = " + fac);
+    }
+}
