aba51ff6b3343daed1b42fa70d20d9c3777dd8df
Prenaka <karma.prenaka@gmail.com>
2020-05-13 18:37:01 -0400

    Calculate

---
+++ Assignment5/Ques1.java
@@ -0,0 +1,19 @@
+public class Ques1{
+
+public static void main(String[]args){
+
+int a=5;
+int b=4;
+int sum=a+b;
+int sub=a-b;
+int mul=a*b;
+int mod=a%b;
+double div=a/b;
+
+System.out.println(sum);
+System.out.println(sub);
+System.out.println(mul);
+System.out.println(mod);
+System.out.println(div);
+}
+}
