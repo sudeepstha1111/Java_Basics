452a85b0856974854fcabcf50e12551135cb52df
lamaningma <lamaninma904@gmail.com>
2020-05-14 18:24:41 +0545

    assignment8

---
+++ Assignment8/Ningma/intmethod.java
@@ -0,0 +1,22 @@
+public class intmethod{
+public static void main(String[] args)
+{ 
+
+int a = 146; int b = 5;
+hi();
+divide(146,5);
+
+
+}
+public static void divide(int a, int b){
+System.out.println(a/b);
+
+}
+public static void hi(){
+System.out.println("The Remainder is = " + "    ");
+
+
+}
+
+
+}
