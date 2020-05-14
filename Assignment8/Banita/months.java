2d47f7acfd68fb192829d167f125522dfd7ad897
banitapou <64861957+banitapou@users.noreply.github.com>
2020-05-14 15:50:34 -0400

    assignment8

--- Assignment8/Banita/months.java
+++ Assignment8/Banita/months.java
@@ -3,32 +3,45 @@ public class months {
         System.out.println("Total number of Months is 12");
         month(); }
     public static void month(){
-        int months=1;
+        int months=3;
         switch (months) {
             case 1:
                 System.out.println("January");
+                break;
             case 2:
                 System.out.println("February");
+                break;
             case 3:
                 System.out.println("March");
+                break;
             case 4:
                 System.out.println("April");
+                break;
             case 5:
                 System.out.println("May");
+                break;
             case 6:
                 System.out.println("June");
+                break;
             case 7:
                 System.out.println("July");
+                break;
             case 8:
                 System.out.println("August");
+                break;
             case 9:
                 System.out.println("September");
+                break;
             case 10:
                 System.out.println("October");
+                break;
             case 11:
                 System.out.println("November");
+                break;
             case 12:
                 System.out.println("December");
+                break;
+            default: System.out.println("case default");
         }
     }
 }
