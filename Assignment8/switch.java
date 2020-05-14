5940c67a02e7e4c34f83d8f7972d92099e0ffb3b
lamaningma <lamaninma904@gmail.com>
2020-05-14 11:54:29 +0545

    Assignment 8

---
+++ Assignment8/switch.java
@@ -0,0 +1,54 @@
+public class Switch {
+
+	public static void main(String[] args) {
+
+			System.out.println("The Total number of Months is 12");
+			month();
+			} 
+
+			public static void month(){
+
+			int months=1;
+			switch (months) {
+			  case 1:
+			    System.out.println("January");
+			   
+			  case 2:
+			    System.out.println("February");
+			
+			  case 3:
+			    System.out.println("March");
+		
+			  case 4:
+			    System.out.println("April");
+			
+			  case 5:
+			    System.out.println("May");
+			   
+			  case 6:
+			    System.out.println("June");
+			  
+			  case 7: 
+			    System.out.println("July");
+			
+			  case 8:
+			    System.out.println("August");
+			 
+			 case 9:
+			    System.out.println("September");
+			    
+			 case 10:
+			    System.out.println("October");
+			  
+			 case 11:
+			    System.out.println("November");
+		
+			 case 12:
+			    System.out.println("December");
+			 
+			}
+
+	}
+
+}
+
