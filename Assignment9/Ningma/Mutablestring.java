a3776b430281b5367d7e757b0e86b3ffa2139bf0
lamaningma <lamaninma904@gmail.com>
2020-05-16 04:32:15 +0545

    mutable

---
+++ Assignment9/Ningma/Mutablestring.java
@@ -0,0 +1,12 @@
+public class Mutablestring{
+public static void main(String[] args){ 
+
+StringBuilder sb = new StringBuilder("love Java");
+
+System.out.println("sb: "+sb);
+sb.append("Java is fun");
+
+}
+
+
+}
