55daed2d2a19d723ab17447a946a3e56f4c29921
blockofcodes <mrshahroshan@gmail.comcleargit config --global user.email mrshahroshan@gmail.com>
2020-05-15 19:02:21 -0400

    Assignment 9

---
+++ Assignment9/StringLiteral.java
@@ -0,0 +1,15 @@
+public class StringLiteral{
+
+	public static void main(String[] args) {
+         String s1 = "Sachin";
+         String s2 = "Sachin";
+         String s3 = new String("Sachin");
+         String s4 = "Sachina";
+         
+         System.out.println(s1.equals(s2));
+         System.out.println(s1.equals(s3));
+
+        System.out.println(s1.equals(s4));
+
+	}
+}
