31ffda22ddc3f10a3b415ac0f2b0302184ac8249
lamaningma <lamaninma904@gmail.com>
2020-05-16 03:15:10 +0545

    Addstring

---
+++ Assignment9/Ningma/Addstring.java
@@ -0,0 +1,13 @@
+public class Addstring{
+public static void main(String[] args){ 
+
+String firstname = "Ashi";
+String lastname = "Beck";
+
+String fullname = firstname + " " + lastname;
+System.out.println(fullname);
+
+}
+
+
+}
