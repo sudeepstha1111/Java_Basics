601d4e3122fbaade38fba6dc9196f6c82fa07378
sudeepstha1111 <sudeep.stha1111@gmail.com>
2020-05-19 23:04:33 -0400

    new changes

---
+++ Assignment5/ExecutionFlow.java
@@ -0,0 +1,28 @@
+public class ExecutionFlow{
+
+//it is a comment line. this is my main method
+public static void main(String args[]){
+
+System.out.println("This is inside main method");
+method1();
+System.out.println("This is after method 1");
+System.out.println("this is the end");
+
+method3();
+method2();
+}
+
+public static void method1(){
+
+System.out.println("This is method 1");
+}
+
+public static void method2(){
+
+System.out.println("This is method 2");
+}
+public static void method3(){
+
+System.out.println("This is method 3");
+}
+}
---
+++ Assignment5/TestIf.java
@@ -0,0 +1,18 @@
+public class TestIf{
+
+public static void main(String [] args){
+
+int a = 4;
+if(a==5){
+
+System.out.println("I am A ");
+
+} else {
+
+
+System.out.println("I am B ");
+}
+
+}
+
+}
