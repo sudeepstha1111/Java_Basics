b76cd0f2a98ab9a755dc1ca26b7e58fcbf99e1a5
keshab034 <64805737+keshab034@users.noreply.github.com>
2020-05-21 18:24:32 -0400

    Assignment 11

---
+++ K-A11.java
@@ -0,0 +1,61 @@
+package assingement.elaven;
+
+public class Shape {
+
+public void properties( ) {
+System.out.println("Area of figure is properly maintained.");
+}
+}
+
+package assingement.elaven;
+public  class Traingle extends Shape {
+}
+
+package assingement.elaven;
+public class Square extends Shape {
+}
+
+package assingement.elaven;
+public class Rectangle extends Shape {
+public static void main(String[] args) {
+		
+Traingle traingleSpace = new Traingle ( );
+Square squareSpace = new Square ( );
+Rectangle rectangleSpace = new Rectangle ( );
+		
+traingleSpace.properties;
+squareSpace.properties;
+rectangleSpace.properties;
+
+
+// Output
+
+Area of figure is properly maintained.
+Area of figure is properly maintained.
+Area of figure is properly maintained.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
