435b7be7787e45f3719d0537d316cce8cb910081
blockofcodes <mrshahroshan@gmail.comcleargit config --global user.email mrshahroshan@gmail.com>
2020-05-22 18:58:31 -0400

    Assignment 12 Roshan

---
+++ Assignment12/AbstractBuilding.java
@@ -0,0 +1,14 @@
+package JavaBasics;
+
+public abstract class AbstractBuilding {
+
+	public abstract void Building();
+	public abstract void floor();
+	public abstract void Room();
+
+	
+	public void Travel(){
+		System.out.println("I Travel to work by train");
+	}
+}
+
