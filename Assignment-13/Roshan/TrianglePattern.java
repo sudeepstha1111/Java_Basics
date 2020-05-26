2ec91a194fe214cf7cfeb3d8135e86edb2009b7b
blockofcodes <mrshahroshan@gmail.comcleargit config --global user.email mrshahroshan@gmail.com>
2020-05-26 18:43:27 -0400

    Triangle Pattern

---
+++ Assignment-13/Roshan/TrianglePattern.java
@@ -0,0 +1,19 @@
+public class TrianglePattern {
+
+	public static void main(String[] args) {
+		 
+			 int i, j, k=1;     
+			 for(i=0; i<6; i++)      
+			 {           
+			 for(j=0; j<k; j++)          
+			 {              
+			  System.out.print("*");  
+			   
+			 }
+		
+			 k = k + 1;
+			 System.out.println(); 
+			 
+			 }
+			}
+			}
